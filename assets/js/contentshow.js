67078b07e2606129abe3b901c3c7016c9604d443
firekilin <wl00161839@gmail.com>
2019-04-06 20:28:34 +0800

    Add files via upload

---
+++ assets/js/contentshow.js
@@ -0,0 +1,46 @@
+function content_open() {
+    document.getElementsByName("showcontent")[0].style.width="calc(100% - 20px)";
+    document.getElementsByName("showcontent")[0].style.overflow="overlay";
+	document.getElementsByName("showcontent")[0].style.height="450px";
+    document.getElementsByClassName("showall")[0].style.display="none";
+}
+
+function change_img(x) {
+    document.getElementsByClassName("showing_img")[0].src=document.getElementsByClassName("content_imgs")[x].src;
+}
+
+var slid=0;
+function change_box(x) {
+    slid+=x;
+    if(slid>=document.getElementsByClassName("content_imgs").length) {
+        slid=0;
+    }
+    else if(slid<0) {
+        slid=(document.getElementsByClassName("content_imgs").length-1);
+    }
+    box_go(slid);
+}
+
+var imgss=["zero",
+"one",
+"two",
+"three",
+"four",
+"five"];
+function box_go(slid) {
+    for(var i=0;
+    i<document.getElementsByClassName("content_imgs").length;
+    i++) {
+        document.getElementsByClassName("content_imgs")[i].className="content_imgs";
+    }
+    for(var i=0;
+    i<6;
+    i++) {
+        if((slid+i)>=(document.getElementsByClassName("content_imgs").length)) {
+            document.getElementsByClassName("content_imgs")[slid+i-document.getElementsByClassName("content_imgs").length].className="content_imgs "+imgss[i];
+        }
+        else {
+            document.getElementsByClassName("content_imgs")[slid+i].className="content_imgs "+imgss[i];
+        }
+    }
+}
